a04c4ce8c1910ba77c46defa20bb419833c94fef
Felix Favour <felixfavour@users.noreply.github.com>
2019-12-17 12:17:15 +0100

    [GCI-chinemerem] added two api endpoints and JSON data for categories and products

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+let categories = require('./my-app-react/src/data/categories.json')
+let products = require('./my-app-react/src/data/products.json')
+
 const express = require('express')
 const app = express()
 
@@ -10,6 +13,37 @@ app.get('/info', (req, res) => {
     res.json(info)
 })
 
+app.get('/products/all', (req, res) => {
+    res.json(products)
+})
+
+app.get('/products/:id', (req, res) => {
+    let urlProductID = req.params.id
+    let productsLocal = products.products
+
+    for (productID in productsLocal) {
+        let product = productsLocal[productID];
+        if(urlProductID == product.id) {
+            res.json(product)
+        }
+    }
+
+})
+
+app.get('/category/:id', (req, res) => {
+    let urlCategoryID = req.params.id
+    let productsLocal = products.products
+    let arrayOfProducts = []
+
+    for (productID in productsLocal) {
+        let product = productsLocal[productID];
+        if(urlCategoryID == product.categoryId) {
+            arrayOfProducts.push(product)
+        }
+    }
+    
+    res.json(arrayOfProducts)
+})
+
 const port = 5000
-   
 app.listen(port, () => console.log(`SERVER started on port ${port}`));
